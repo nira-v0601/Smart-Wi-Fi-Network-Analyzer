1fdff26e32efb149312fcf226375c44d3d7cb0b3
Dev Patel <devpatelthe@gmail.com>
2026-05-02 10:01:37 +0530

    Changes by Dev

--- packages/flutter_internet_speed_test_plus/example/android/app/src/main/kotlin/com/shaz/plugin/fist/flutter_internet_speed_test_plus_example/MainActivity.kt
+++ packages/flutter_internet_speed_test_plus/example/android/app/src/main/kotlin/com/shaz/plugin/fist/flutter_internet_speed_test_plus_example/MainActivity.kt
@@ -2,5 +2,4 @@ package com.shaz.plugin.fist.flutter_internet_speed_test_plus_example
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-}
+class MainActivity : FlutterActivity()
